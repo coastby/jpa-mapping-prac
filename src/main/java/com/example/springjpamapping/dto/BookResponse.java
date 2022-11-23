b9691a49455499fa174a78e7455404f68aea2ade
JO YEJI <whdpwl2@gmail.com>
2022-11-23 14:51:25 +0900

    Refactor: BookResponse builder 패턴으로 변경

--- src/main/java/com/example/springjpamapping/dto/BookResponse.java
+++ src/main/java/com/example/springjpamapping/dto/BookResponse.java
@@ -3,16 +3,22 @@ package com.example.springjpamapping.dto;
 import com.example.springjpamapping.domain.Author;
 import com.example.springjpamapping.domain.Book;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class BookResponse {
     private Long id;
     private String name;
     private String author;
 
     public static BookResponse of(Book book, Author author){
-        return new BookResponse(book.getId(), book.getName(), author.getName());
+        return BookResponse.builder()
+                .id(book.getId())
+                .name(book.getName())
+                .author(author.getName())
+                .build();
     }
 }
