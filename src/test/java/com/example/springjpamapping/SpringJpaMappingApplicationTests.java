41fe89f25327f923091307e240ee1e9cee85dfab
JO YEJI <whdpwl2@gmail.com>
2022-11-23 14:39:16 +0900

    Test: test 삭제

--- src/test/java/com/example/springjpamapping/SpringJpaMappingApplicationTests.java
+++ src/test/java/com/example/springjpamapping/SpringJpaMappingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJpaMappingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
